71f3f0696029ec38d2713101106889fa4b035c13
Joey Rudd <emailjoeyrudd@gmail.com>
2025-07-18 12:12:47 -0400

    Completed quiz 1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -11,6 +11,13 @@
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(apple_quantity: u32) -> u32 {
+    if apple_quantity <= 40 {
+        apple_quantity * 2
+    } else {
+        apple_quantity
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
