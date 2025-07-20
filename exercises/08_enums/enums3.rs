f14a13dfa9aa581664e9b033b73ab0499ac6472c
Joey Rudd <emailjoeyrudd@gmail.com>
2025-07-20 15:45:55 -0400

    Completed enums3 exercise, therefore completing all exercises for enums

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -46,6 +46,16 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Resize { width, height } => {
+                self.width = width;
+                self.height = height;
+            }
+            Message::ChangeColor(red, green, blue) => self.color = (red, green, blue),
+            Message::Echo(s) => self.message = s,
+            Message::Move(Point { x, y }) => self.position = Point { x, y },
+            Message::Quit => self.quit(),
+        };
     }
 }
 
